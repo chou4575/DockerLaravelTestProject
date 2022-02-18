a1cd3b048b3807cd73236b1fa60beeea33222b3d
chou4575 <chou@canal.bz>
2022-02-18 13:04:14 +0900

    database complete

---
+++ backend/app/Http/Controllers/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\KrononUser;
+use Faker\Provider\ar_JO\Person;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class HelloController extends Controller
+{
+    public function index()
+    {
+        $items = DB::table('people')->get();
+        return view('hello.hello',['items' => $items]);
+    }
+}
